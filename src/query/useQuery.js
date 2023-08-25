6e3039b8958e7e8d0d0854aa68612881ff41d842
IbrahimMohamed2022 <114517992+IbrahimMohamed2022@users.noreply.github.com>
2023-08-26 01:16:55 +0300

    Add files via upload

---
+++ src/query/useQuery.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react'
+import * as BooksAPI from '../BooksAPI'
+
+import { useDebounce } from 'use-debounce';
+
+
+function useQuery(query) {
+
+    const [searchBooks, setSearchBooks] = useState([]);
+    const [value] = useDebounce(query, 500);
+
+    useEffect(() => {
+
+        let isActive = true;
+        if (value) {
+            BooksAPI.search(value).then(data => {
+                if (data.error) {
+                    setSearchBooks([])
+                } else {
+                    if (isActive) {
+                        setSearchBooks(data);
+                    }
+                }
+            })
+        }
+        return () => {
+            isActive = false;
+            setSearchBooks([])
+        }
+
+    }, [value])
+
+
+    return [searchBooks, setSearchBooks];
+
+}
+
+export default useQuery
